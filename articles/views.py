434b2dad2e298c7c3af45551f4e6fff1be172d91
Ccepero <cindalis.cepero@newbury.edu>
2018-12-04 02:27:03 -0500

    final steps with added permissions

--- articles/views.py
+++ articles/views.py
@@ -1,12 +1,9 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-from . import models
 
-class ArticleCreateView(CreateView):
-    model = models.Article
-    template_name = 'article_new.html'
-    fields = ['title', 'body', 'author',]
+from . import models
 
 class ArticleListView(ListView):
     model = models.Article
@@ -26,4 +23,14 @@ class ArticleDeleteView(DeleteView):
     model = models.Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
+    model = models.Article
+    template_name = 'article_new.html'
+    fields = ['title', 'body',]
+    login_url = 'login'
+
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
